8605f7ef4be0b814997179cd48c434223243e0d8
Dean Shaff <dshaff001@gmail.com>
2017-06-06 12:18:00 +0400

    Changed version in __init__.py

--- uninstall.sh
+++ uninstall.sh
@@ -6,11 +6,11 @@ if [[ $INIT_FILE == "git_pythonpath/__init__.pyc" ]]; then
 	if [[ `pwd` != $HOME ]]; then
 		cd $HOME # Hope it's not installed in home directory
 	else
-		cd .. # Less ideal is moving into some random directory 
+		cd .. # Less ideal is moving into some random directory
 	fi
 	INIT_FILE=`python -c "import git_pythonpath; print(git_pythonpath.__file__)"`
-fi 
-# Do one final check to make sure we actually have the egg file 
+fi
+# Do one final check to make sure we actually have the egg file
 if [[ $INIT_FILE == *"git_pythonpath-$MODULE_VERSION-py2.7.egg"* ]]; then
 	echo "Removing egg file"
 	MODULE_EGG=$(dirname "$INIT_FILE")
@@ -19,5 +19,5 @@ if [[ $INIT_FILE == *"git_pythonpath-$MODULE_VERSION-py2.7.egg"* ]]; then
 else
 	echo "Couldn't safely remove package"
 	echo "Look for .egg files in output of the following command"
-	echo "python setup.py install --record" 
-fi 
+	echo "python setup.py install --record"
+fi
